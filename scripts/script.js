7032b1e3e1854366ff03158cf26512e65125bf2f
thoranderskow <tanderskow@gmail.com>
2020-02-24 15:19:29 -0600

    initialized project

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function panel1_handler() {
+    document.getElementById('panel1').style.backgroundColor = "#fa4";
+}
+function panel2_handler() {
+    document.getElementById('panel2').style.backgroundColor = "#Fa9";
+}
+function panel3_handler() {
+    document.getElementById('panel3').style.backgroundColor = "#f88";
+}
+function panel4_handler() {
+    document.getElementById('panel4').style.backgroundColor = "#fe1";
+}
