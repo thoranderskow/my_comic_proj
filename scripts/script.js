f1eb49190511c2b91f82d3f35c52bb6d43ef8d4a
thoranderskow <tanderskow@gmail.com>
2020-03-07 13:01:47 -0600

    almost done

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 let html_reset = "<img id='img' style='display: none'> <div id='panel1' class='panel' onclick='panel1_handler()'> </div> <div id='panel2' class='panel' onclick='panel2_handler()'> </div> <div id='panel3' class='panel' onclick='panel3_handler()'> </div> <div id='panel4' class='panel' onclick='panel4_handler()'> </div>";
-let reveal1 = 0;
+let end_screen = "<img id='img' style='display: none'> <a id='link1'><div id='panel1' class='panel'> </div></a><a id='link2'> <div id='panel2' class='panel'> </div></a><a id='link3'> <div id='panel3' class='panel'> </div> </a><a id='link4'><div id='panel4' class='panel'> </div></a><a id='link5'> <div id='panel5' class='panel'>  </div> </a><a id='link6'> <div id='panel6' class='panel'>  </div></a>";
 let reveal2 = 0;
 let reveal3 = 0;
 let reveal4 = 0;
 let loops = 0;
-let quotes = [['Instagram.', 'Vanity?', 'Confidence.', 'Affirmation.'], ['LinkedIn.', 'Success.', 'Ambition.', 'Business.'],['Facebook.', 'Comparison.', 'Updates.', 'Info.']];
-let images = ['images/girlatbeach.png','images/professional.png','images/fb.png' ]
+let quotes = [['Instagram.', 'Vanity?', 'Confidence.', 'Affirmation.'], ['LinkedIn.', 'Success.', 'Ambition.', 'Business.'],['Facebook.', 'Comparison.', 'Updates.', 'Info.'],['Reddit.', 'Hivemind.', 'Cultivated.', 'News.'], ['Youtube.', 'Sanitized.', 'Ads.', 'Monetization.'],['Email.', 'Identifier.', 'Correspondence.', 'Footprint.'],['You.', 'On.', 'The.', 'Web.']];
+let images = ['images/girlatbeach.png','images/professional.png','images/fb.png','images/reddit.png', 'images/youtube.png', 'images/email.png' ]
+let links = ['http://www.instagram.com', 'http://www.linkedin.com', 'http://www.facebook.com', 'http://www.reddit.com', 'http://www.youtube.com', 'http://www.gmail.com']
 
 function reset() {
   let p = document.getElementById('scr');
@@ -15,6 +16,7 @@ function reset() {
   reveal4 = 0;
   loops += 1;
   p.innerHTML = html_reset;
+  document.getElementById('scr').style = ""
 }
 
 function show_img(image){
@@ -23,6 +25,7 @@ function show_img(image){
   let img = document.getElementById('img')
   img.src = image;
   img.style = "width: 100%; height: 100%; display: block; border-radius: 25px;"
+  document.getElementById('scr').style = "height: 30vw;"
   setTimeout(() => {  reset(); }, 3000);
 }
 
@@ -34,7 +37,19 @@ function set_image() {
     p.style.display = "none";
     p.onclick = "";
   }
-  show_img(images[loops]);
+  if (loops == 6) {
+    document.getElementById('body').innerHTML = end_screen;
+    for (let i = 1; i < 7; i++){
+      let element = "panel" + i;
+      let p = document.getElementById(element);
+      let img = images[i-1];
+      p.style.backgroundImage = "url('"+img+"')";
+      document.getElementById('link'+i).href = links[i-1];
+
+    }
+  } else {
+    show_img(images[loops]);
+  }
 }
 
 function get_strings(num, loop) {
